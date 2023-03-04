7d87a1f8a7fb7b00b133813c9cb0a87f3c6b3cb0
Nytri <llTheBlankll@gmail.com>
2023-03-04 22:37:59 +0800

    Connection created

---
+++ Student Information System/Connection.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Student_Information_System;
+
+public abstract class Connection
+{
+
+    private const string Server = @"localhost";
+    private const string Username = @"public";
+    private const string Password = @"public";
+    private const string Database = @"student_information";
+
+    public static MySqlConnection GetMySqlConnection()
+    {
+        var connection = new MySqlConnection();
+        connection.ConnectionString = @"server=" + Server + ";username=" + Username + ";password=" + Password +
+                                      ";database=" + Database;
+        
+        try
+        {
+
+            if (connection.State.Equals(ConnectionState.Closed))
+            {
+                connection.Open();
+                return connection;
+            }
+        }
+        catch (MySqlException e)
+        {
+            MessageBox.Show(e.Message);
+        }
+
+        return connection;
+    }
+}
