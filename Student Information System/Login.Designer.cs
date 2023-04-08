ada14cf9c466ecf5e400a7d1330c321c847e2f8c
Nytri <llTheBlankll@gmail.com>
2023-04-08 20:47:47 +0800

    Login connection

--- Student Information System/Login.Designer.cs
+++ Student Information System/Login.Designer.cs
@@ -28,111 +28,111 @@
         /// </summary>
         private void InitializeComponent ()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtbox_username = new System.Windows.Forms.TextBox();
-            this.txtbox_password = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btn_signin = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
+            label1 =  new Label() ;
+            panel1 =  new Panel() ;
+            label2 =  new Label() ;
+            txtbox_username =  new TextBox() ;
+            txtbox_password =  new TextBox() ;
+            label3 =  new Label() ;
+            btn_signin =  new Button() ;
+            panel1.SuspendLayout();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(153, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 38);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Sign in";
+            label1.AutoSize =  true ;
+            label1.Font =  new Font( "Century Gothic", 24F, FontStyle.Bold, GraphicsUnit.Point ) ;
+            label1.ForeColor =  Color.White ;
+            label1.Location =  new Point( 153, 27 ) ;
+            label1.Name =  "label1" ;
+            label1.Size =  new Size( 118, 38 ) ;
+            label1.TabIndex =  0 ;
+            label1.Text =  "Sign in" ;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Gray;
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(434, 96);
-            this.panel1.TabIndex = 1;
+            panel1.BackColor =  Color.Gray ;
+            panel1.Controls.Add( label1 );
+            panel1.Dock =  DockStyle.Top ;
+            panel1.Location =  new Point( 0, 0 ) ;
+            panel1.Name =  "panel1" ;
+            panel1.Size =  new Size( 434, 96 ) ;
+            panel1.TabIndex =  1 ;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(160, 167);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 22);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Username";
+            label2.AutoSize =  true ;
+            label2.Location =  new Point( 160, 167 ) ;
+            label2.Name =  "label2" ;
+            label2.Size =  new Size( 101, 22 ) ;
+            label2.TabIndex =  2 ;
+            label2.Text =  "Username" ;
             // 
             // txtbox_username
             // 
-            this.txtbox_username.Location = new System.Drawing.Point(64, 205);
-            this.txtbox_username.MaxLength = 32;
-            this.txtbox_username.Name = "txtbox_username";
-            this.txtbox_username.PlaceholderText = "Enter your username";
-            this.txtbox_username.Size = new System.Drawing.Size(300, 31);
-            this.txtbox_username.TabIndex = 3;
-            this.txtbox_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            txtbox_username.Location =  new Point( 64, 205 ) ;
+            txtbox_username.MaxLength =  32 ;
+            txtbox_username.Name =  "txtbox_username" ;
+            txtbox_username.PlaceholderText =  "Enter your username" ;
+            txtbox_username.Size =  new Size( 300, 31 ) ;
+            txtbox_username.TabIndex =  3 ;
+            txtbox_username.TextAlign =  HorizontalAlignment.Center ;
             // 
             // txtbox_password
             // 
-            this.txtbox_password.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtbox_password.Location = new System.Drawing.Point(64, 305);
-            this.txtbox_password.MaxLength = 32;
-            this.txtbox_password.Name = "txtbox_password";
-            this.txtbox_password.PasswordChar = '*';
-            this.txtbox_password.PlaceholderText = "Enter your password";
-            this.txtbox_password.Size = new System.Drawing.Size(300, 30);
-            this.txtbox_password.TabIndex = 5;
-            this.txtbox_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.txtbox_password.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbox_password_KeyPress);
+            txtbox_password.Font =  new Font( "Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point ) ;
+            txtbox_password.Location =  new Point( 64, 305 ) ;
+            txtbox_password.MaxLength =  32 ;
+            txtbox_password.Name =  "txtbox_password" ;
+            txtbox_password.PasswordChar =  '*' ;
+            txtbox_password.PlaceholderText =  "Enter your password" ;
+            txtbox_password.Size =  new Size( 300, 30 ) ;
+            txtbox_password.TabIndex =  5 ;
+            txtbox_password.TextAlign =  HorizontalAlignment.Center ;
+            txtbox_password.KeyPress +=  txtbox_password_KeyPress ;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(160, 267);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 22);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Password";
+            label3.AutoSize =  true ;
+            label3.Location =  new Point( 166, 267 ) ;
+            label3.Name =  "label3" ;
+            label3.Size =  new Size( 95, 22 ) ;
+            label3.TabIndex =  4 ;
+            label3.Text =  "Password" ;
             // 
             // btn_signin
             // 
-            this.btn_signin.Font = new System.Drawing.Font("Leelawadee", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_signin.Location = new System.Drawing.Point(153, 353);
-            this.btn_signin.Name = "btn_signin";
-            this.btn_signin.Size = new System.Drawing.Size(111, 34);
-            this.btn_signin.TabIndex = 6;
-            this.btn_signin.Text = "Sign in";
-            this.btn_signin.UseVisualStyleBackColor = true;
-            this.btn_signin.Click += new System.EventHandler(this.btn_signin_Click);
+            btn_signin.Font =  new Font( "Leelawadee", 14.25F, FontStyle.Regular, GraphicsUnit.Point ) ;
+            btn_signin.Location =  new Point( 153, 353 ) ;
+            btn_signin.Name =  "btn_signin" ;
+            btn_signin.Size =  new Size( 111, 34 ) ;
+            btn_signin.TabIndex =  6 ;
+            btn_signin.Text =  "Sign in" ;
+            btn_signin.UseVisualStyleBackColor =  true ;
+            btn_signin.Click +=  btn_signin_Click ;
             // 
-            // Login_Form
+            // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 461);
-            this.Controls.Add(this.btn_signin);
-            this.Controls.Add(this.txtbox_password);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtbox_username);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.panel1);
-            this.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(5);
-            this.Name = "LoginForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions =  new SizeF( 11F, 22F ) ;
+            AutoScaleMode =  AutoScaleMode.Font ;
+            BackColor =  Color.White ;
+            ClientSize =  new Size( 434, 461 ) ;
+            Controls.Add( btn_signin );
+            Controls.Add( txtbox_password );
+            Controls.Add( label3 );
+            Controls.Add( txtbox_username );
+            Controls.Add( label2 );
+            Controls.Add( panel1 );
+            Font =  new Font( "Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point ) ;
+            FormBorderStyle =  FormBorderStyle.FixedSingle ;
+            Margin =  new Padding( 5 ) ;
+            Name =  "LoginForm" ;
+            StartPosition =  FormStartPosition.CenterScreen ;
+            Text =  "Form1" ;
+            panel1.ResumeLayout( false );
+            panel1.PerformLayout();
+            ResumeLayout( false );
+            PerformLayout();
         }
 
         #endregion
--- Student Information System/Login.cs
+++ Student Information System/Login.cs
@@ -4,63 +4,65 @@ namespace Student_Information_System
 {
     public partial class LoginForm : Form
     {
-        public LoginForm()
+        public LoginForm ()
         {
             InitializeComponent();
         }
 
-        private void btn_signin_Click(object sender, EventArgs e)
+        private void btn_signin_Click ( object sender, EventArgs e )
         {
-            if (this.txtbox_username.Text.Equals(""))
+            if ( this.txtbox_username.Text.Equals( "" ) )
             {
-                MessageBox.Show(@"Username field cannot be empty.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-            
-            if (this.txtbox_password.Text.Equals(""))
-            {
-                MessageBox.Show(@"Username field cannot be empty.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show( @"Username field cannot be empty.", "", MessageBoxButtons.OK, MessageBoxIcon.Error );
                 return;
             }
 
-            try
+            if ( this.txtbox_password.Text.Equals( "" ) )
             {
-                MySqlConnection connection = Connection.GetMySqlConnection();
-                connection.Open();
-
-                const string statement =
-                    "SELECT * FROM users WHERE BINARY username = @username AND BINARY password = @password";
-                MySqlCommand command = new MySqlCommand(statement, connection);
-                command.Parameters.AddWithValue("@username", txtbox_username.Text);
-                command.Parameters.AddWithValue("@password", txtbox_password.Text);
-
-                using (MySqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        if (reader.GetString("username").Equals(txtbox_username.Text) &&
-                            reader.GetString("password").Equals(txtbox_password.Text))
-                        {
-                            new Dashboard.Dashboard().Show();
-                        }
-                    }
-                }
-                
-                connection.Close();
+                MessageBox.Show( @"Username field cannot be empty.", "", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                return;
             }
-            catch (MySqlException exception)
-            {
-                MessageBox.Show(exception.Message);
-            }
+
+            new Dashboard.Dashboard().Show();
+
+            //try
+            //{
+            //    MySqlConnection connection = Connection.GetMySqlConnection();
+            //    connection.Open();
+
+            //    const string statement =
+            //        "SELECT * FROM users WHERE BINARY username = @username AND BINARY password = @password";
+            //    MySqlCommand command = new MySqlCommand( statement, connection );
+            //    command.Parameters.AddWithValue( "@username", txtbox_username.Text );
+            //    command.Parameters.AddWithValue( "@password", txtbox_password.Text );
+
+            //    using ( MySqlDataReader reader = command.ExecuteReader() )
+            //    {
+            //        while ( reader.Read() )
+            //        {
+            //            if ( reader.GetString( "username" ).Equals( txtbox_username.Text ) &&
+            //                reader.GetString( "password" ).Equals( txtbox_password.Text ) )
+            //            {
+            //                new Dashboard.Dashboard().Show();
+            //            }
+            //        }
+            //    }
+
+            //    connection.Close();
+            //}
+            //catch ( MySqlException exception )
+            //{
+            //    MessageBox.Show( exception.Message );
+            //}
         }
 
-        private void txtbox_password_KeyPress(object sender, KeyPressEventArgs e)
+        private void txtbox_password_KeyPress ( object sender, KeyPressEventArgs e )
         {
-            if (e.KeyChar == (char)Keys.Enter)
+            if ( e.KeyChar == (char) Keys.Enter )
             {
                 btn_signin.PerformClick();
             }
-            
+
         }
     }
 }
