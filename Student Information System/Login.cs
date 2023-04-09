51e861a0158950092298e60a64226cdca75d7017
klkl <lj@W.com>
2023-04-09 09:36:51 +0800

    can log in now to database

--- Student Information System/Login.cs
+++ Student Information System/Login.cs
@@ -23,37 +23,35 @@ namespace Student_Information_System
                 return;
             }
 
-            new Dashboard.Dashboard().Show();
+            try
+            {
+                MySqlConnection connection = Connection.GetMySqlConnection();
+                connection.Open();
 
-            //try
-            //{
-            //    MySqlConnection connection = Connection.GetMySqlConnection();
-            //    connection.Open();
+                const string statement =
+                    "SELECT * FROM users WHERE BINARY username = @username AND BINARY password = @password";
+                MySqlCommand command = new MySqlCommand( statement, connection );
+                command.Parameters.AddWithValue( "@username", txtbox_username.Text );
+                command.Parameters.AddWithValue( "@password", txtbox_password.Text );
 
-            //    const string statement =
-            //        "SELECT * FROM users WHERE BINARY username = @username AND BINARY password = @password";
-            //    MySqlCommand command = new MySqlCommand( statement, connection );
-            //    command.Parameters.AddWithValue( "@username", txtbox_username.Text );
-            //    command.Parameters.AddWithValue( "@password", txtbox_password.Text );
+                using ( MySqlDataReader reader = command.ExecuteReader() )
+                {
+                    while ( reader.Read() )
+                    {
+                        if ( reader.GetString( "username" ).Equals( txtbox_username.Text ) &&
+                            reader.GetString( "password" ).Equals( txtbox_password.Text ) )
+                        {
+                            new Dashboard.Dashboard().Show();
+                        }
+                    }
+                }
 
-            //    using ( MySqlDataReader reader = command.ExecuteReader() )
-            //    {
-            //        while ( reader.Read() )
-            //        {
-            //            if ( reader.GetString( "username" ).Equals( txtbox_username.Text ) &&
-            //                reader.GetString( "password" ).Equals( txtbox_password.Text ) )
-            //            {
-            //                new Dashboard.Dashboard().Show();
-            //            }
-            //        }
-            //    }
-
-            //    connection.Close();
-            //}
-            //catch ( MySqlException exception )
-            //{
-            //    MessageBox.Show( exception.Message );
-            //}
+                connection.Close();
+            }
+            catch ( MySqlException exception )
+            {
+                MessageBox.Show( exception.Message );
+            }
         }
 
         private void txtbox_password_KeyPress ( object sender, KeyPressEventArgs e )
